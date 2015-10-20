71d2fc3941620ff84e7cbc80dfe2128e60bbacb0
James Cool <jeffreysbrother@gmail.com>
2015-10-20 14:49:45 -0700

    added max-width property and normalize css

--- wp-content/themes/learningWordPress/functions.php
+++ wp-content/themes/learningWordPress/functions.php
@@ -1,7 +1,8 @@
 <?php
 
 function learningWordPress_resources() {
-  wp_enqueue_style('style', get_stylesheet_uri());
+  wp_enqueue_style('normalize', get_template_directory_uri() . '/normalize.css');
+  wp_enqueue_style('style', get_template_directory_uri() . '/style.css');
 }
 
 add_action('wp_enqueue_scripts', 'learningWordPress_resources');
