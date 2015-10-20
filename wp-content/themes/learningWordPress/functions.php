96c1cc1d038009d2de1714c344841537d06a17db
James Cool <jeffreysbrother@gmail.com>
2015-10-20 07:23:18 -0700

    removed rogue Contact Us blurb

--- wp-content/themes/learningWordPress/functions.php
+++ wp-content/themes/learningWordPress/functions.php
@@ -24,3 +24,12 @@ function get_top_ancestor_id() {
 
   return $post->ID;
 }
+
+
+//does page have children
+function has_children() {
+  global $post;
+
+  $pages = get_pages('child_of=' . $post->ID);
+  return count($pages);
+}
