06dde4d878a61942a45765b5ef4d57b55ed76227
James Cool <jeffreysbrother@gmail.com>
2015-10-21 04:44:51 -0700

    added search functionality

--- wp-content/themes/learningWordPress/header.php
+++ wp-content/themes/learningWordPress/header.php
@@ -13,11 +13,15 @@
 
         <!-- site header -->
         <header class="site-header">
+
+          <div class="hd-search">
+            <?php get_search_form(); ?>            
+          </div>
+
           <h1><a href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a></h1>
           <h5><?php bloginfo('description'); ?><?php if(is_page(portfolio)) { ?>
             - Thanks for viewing our work
           <?php }?></h5>
-
 
         <nav class="site-nav">
 
