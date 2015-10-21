96d0add42738b6e00eee18de5a165961e6003b97
James Cool <jeffreysbrother@gmail.com>
2015-10-21 15:22:15 -0700

    output post titles on front page with loop

--- wp-content/themes/learningWordPress/front-page.php
+++ wp-content/themes/learningWordPress/front-page.php
@@ -14,6 +14,21 @@ if(have_posts()) :
 
   endif;
 
+
+  //posts loop begins here
+  $opinionPosts = new WP_Query('cat=5&posts_per_page=2');
+
+  if($opinionPosts->have_posts()) :
+  while($opinionPosts->have_posts()) : $opinionPosts->the_post(); ?>
+  	<h2><?php the_title(); ?></h2>
+  <?php endwhile;
+
+  else :
+   	//fallback to no content message
+  endif;
+  wp_reset_postdata();
+
+
   get_footer();
 
 ?>
