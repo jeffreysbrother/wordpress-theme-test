8ab627054c8cae56b99055704ddaf289e49ed443
James Cool <jeffreysbrother@gmail.com>
2015-10-21 20:27:11 -0700

    page title hidden in small viewports

--- wp-content/themes/learningWordPress/header.php
+++ wp-content/themes/learningWordPress/header.php
@@ -15,13 +15,11 @@
         <header class="site-header">
 
           <div class="hd-search">
-            <?php get_search_form(); ?>            
+            <?php get_search_form(); ?>
           </div>
 
           <h1><a href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a></h1>
-          <h5><?php bloginfo('description'); ?><?php if(is_page(portfolio)) { ?>
-            - Thanks for viewing our work
-          <?php }?></h5>
+          <h5><?php bloginfo('description'); ?></h5>
 
         <nav class="site-nav">
 
