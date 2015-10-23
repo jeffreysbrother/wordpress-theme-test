b68560f234faf60579c90359ecacfb1f785cd20f
James Cool <jeffreysbrother@gmail.com>
2015-10-23 14:51:56 -0700

    made footer responsive

--- wp-content/themes/learningWordPress/footer.php
+++ wp-content/themes/learningWordPress/footer.php
@@ -9,7 +9,7 @@
        <?php wp_nav_menu( $args ); ?>
     </nav>
 
-    <p><?php bloginfo('name'); ?> &copy; <?php echo date('Y') ?></p>
+    <p><?php bloginfo('name');?> &copy; <?php echo date('Y')?></p>
   </footer>
 
 </div>
